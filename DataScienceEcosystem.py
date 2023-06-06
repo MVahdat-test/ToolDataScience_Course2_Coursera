2a63a68e33cf4b74aa6a937e1075ad7e994f9333
MVahdat-test <135665278+MVahdat-test@users.noreply.github.com>
2023-06-06 13:32:25 -0400

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# - Create a Jupyter Notebook with markdown and code cells
+# - list data science languages
+# - list data science libraries
+# - Demonstrate our proficiency in using Markdown in Jupyter notebooks
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R 
+# 3. SQL 
+# 4. Java 
+# 5. Scala
+# 6. Julia
+# 7. C++
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. NumPy
+# 3. Matplotlib
+# 4. Scikit-Learn
+
+# |Data Science Tools| 
+# | -----------------| 
+# |Rstudio|
+# |TensorFlow|
+# |Apache Spark|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python:
+
+# In[8]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[7]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ## Author
+# Mahnaz
+
+# In[ ]:
+
+
+
+
